b6314fb2315716dc1bb6dad54fb89cd5c76b48a1
KataevEvgeniy <jackman.bup@gmail.com>
2022-09-23 17:55:40 +0500

    init

---
+++ src/main/java/taskScheduler/controllers/WelcomeController.java
@@ -0,0 +1,19 @@
+package taskScheduler.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String toWelcomePage() {
+		return "welcome";
+	}
+	
+	@GetMapping("/main")
+	public String toMainPage() {
+		return "main";
+	}
+}
